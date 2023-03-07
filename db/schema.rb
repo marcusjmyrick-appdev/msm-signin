0a526f8d4d51a81ea17c1dffa9552d36e941fb0a
Marcus Myrick <mmyric4@illinois.edu>
2023-03-07 21:32:54 +0000

    save for now

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_03_07_211012) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "director_id"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
